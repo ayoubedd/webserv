44162d56c9076118d611bbd966fff59f1502cf96
Ayoub Eddaoudi <eddaouddi.ayoub@gmail.com>
2023-08-23 16:21:53 +0100

    feat(libhttp): add MultipartFormData header

---
+++ libhttp/MultipartFormData.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "libhttp/Headers.hpp"
+#include <vector>
+
+namespace libhttp {
+  struct MutlipartFormDataEntity {
+    enum error {
+      OK,
+      MALFORMED,
+    };
+    libhttp::Headers headers;
+    std::vector<char> body;
+    static std::vector<libhttp::MutlipartFormDataEntity> decode(const std::vector<char> &src,
+                                                                const std::string &del);
+  };
+} // namespace libhttp
