833e14379baa785caac381463f275216a085e248
houssam abouiba <32068899+0xPride@users.noreply.github.com>
2023-08-26 15:28:27 +0100

    chore: adding client info the the session

--- libhttp/Request.hpp
+++ libhttp/Request.hpp
@@ -3,15 +3,17 @@
 #include "libhttp/Headers.hpp"
 #include "libhttp/Request-target.hpp"
 #include <iostream>
+#include <netinet/ip.h>
 #include <vector>
 
 namespace libhttp {
   struct Request {
-    std::string method;
-    RequestTarget reqTarget;
-    std::string version;
-    Headers headers;
+    std::string       method;
+    RequestTarget     reqTarget;
+    std::string       version;
+    Headers           headers;
     std::vector<char> body;
+    sockaddr_in      *clientAddr;
   };
 } // namespace libhttp
 
--- libnet/Net.cpp
+++ libnet/Net.cpp
@@ -87,7 +87,7 @@ void libnet::Netenv::prepFdSets(void) {
 
 int libnet::Netenv::largestFd(void) {
   libnet::Sessions::iterator sessionsLargestFd = std::max_element(sessions.begin(), sessions.end());
-  libnet::Sockets::iterator socketsLargestFd = std::max_element(sockets.begin(), sockets.end());
+  libnet::Sockets::iterator  socketsLargestFd = std::max_element(sockets.begin(), sockets.end());
 
   return std::max(sessionsLargestFd->first, *socketsLargestFd);
 }
@@ -134,16 +134,20 @@ void libnet::Netenv::awaitEvents(void) {
 void libnet::Netenv::acceptNewClients(void) {
   libnet::Sockets::iterator begin = readReadySockets.begin();
   libnet::Sockets::iterator end = readReadySockets.end();
+  socklen_t                 clientAddrLen;
+  sockaddr_in               clientAddr;
 
   int fd;
+  clientAddrLen = sizeof clientAddr;
   while (begin != end) {
-    if ((fd = accept(*begin, NULL, 0)) == -1) {
+    bzero(&clientAddr, sizeof clientAddr);
+    if ((fd = accept(*begin, (struct sockaddr *)&clientAddr, &clientAddrLen)) == -1) {
       std::cerr << "accept" << strerror(errno) << std::endl;
       return;
     }
 
     // add the new client to sessions pool
-    sessions.insert(std::make_pair(fd, new libnet::Session(fd)));
+    sessions.insert(std::make_pair(fd, new libnet::Session(fd, &clientAddr)));
 
     begin++;
   }
