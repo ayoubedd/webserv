20bba7e9699c10fb8f03188196211a38ba962e30
Ayoub Eddaoudi <eddaouddi.ayoub@gmail.com>
2023-09-17 15:43:49 +0100

    chore(libhttp/session): add TranferEncoding struct to Session struct

--- libnet/Session.hpp
+++ libnet/Session.hpp
@@ -2,6 +2,7 @@
 
 #include "libhttp/Reader.hpp"
 #include "libhttp/Request.hpp"
+#include "libhttp/TransferEncoding.hpp"
 #include "libnet/SessionState.hpp"
 #include <netinet/in.h>
 #include <queue>
@@ -15,6 +16,7 @@ namespace libnet {
     // std::queue<libhttp::Request *> requests;
     // libhttp::Request               request;
     // libnet::SessionState status;
+    libhttp::TransferEncoding transferEncoding;
     libhttp::Reader reader;
     sockaddr_in    *clientAddr;
   };
