2a4c5a8427da2b1cba754828fc4a94a1ef8981ad
Ayoub Eddaoudi <eddaouddi.ayoub@gmail.com>
2023-10-06 14:53:19 +0100

    feat(libhttp/Session): add Writer to Session struct

--- libnet/Session.cpp
+++ libnet/Session.cpp
@@ -1,13 +1,11 @@
 #include "libnet/Session.hpp"
+#include "libhttp/Writer.hpp"
 #include <cstring>
 #include <netinet/ip.h>
 // #include <string.h>
 
 libnet::Session::Session(int fd, sockaddr_in *clientAddr)
     : fd(fd)
-    // , request(libhttp::Request())
-    // , status(READING_HEADERS)
     , reader(fd, *clientAddr)
-    , clientAddr(clientAddr) {
-  // this->request.clientAddr = clientAddr;
-}
+    , writer(fd)
+    , clientAddr(clientAddr) {}
