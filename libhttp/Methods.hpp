83efdf666782d0df8687604b8448a06691dbfe09
Pride <32068899+0xPride@users.noreply.github.com>
2023-10-10 12:01:05 +0000

    chore: adding static values for get post delete

--- libhttp/Methods.hpp
+++ libhttp/Methods.hpp
@@ -1,44 +1,39 @@
 #pragma once
-#include "libparse/Config.hpp"
-#include "libhttp/Request.hpp"
 #include "libhttp/Headers.hpp"
+#include "libhttp/Request.hpp"
 #include "libhttp/Response.hpp"
+#include "libparse/Config.hpp"
 
-#include <utility>
-#include <fcntl.h>
-#include <vector>
 #include <dirent.h>
+#include <fcntl.h>
+#include <string>
 #include <sys/stat.h>
 #include <unistd.h>
-#include <string>
+#include <utility>
+#include <vector>
 
 namespace libhttp {
-    struct Methods {
-    enum error{
-      OK, 
-      FILE_NOT_FOUND,
-      FORBIDDEN
-    };
-    struct file{
+  struct Methods {
+    static const char *GET;
+    static const char *POST;
+    static const char *DELETE;
+
+    enum error { OK, FILE_NOT_FOUND, FORBIDDEN };
+    struct file {
       std::string name;
       std::string date;
-      ssize_t size;
+      ssize_t     size;
     };
-    enum typeFile{
-      FILE,
-      DIR,
-      NOT_FOUND
-    };
+    enum typeFile { FILE, DIR, NOT_FOUND };
     error err;
     struct GetRes {
-      int fd;
-      std::vector<char > headers;
-      std::pair<int,int> range;
+      int                 fd;
+      std::vector<char >  headers;
+      std::pair<int, int> range;
     };
-
- };    
-  std::pair<Methods::error,Response > Get(Request &request, std::string path);
-  std::pair<Methods::error,Response > Delete(std::string path);
-  ssize_t getFile(std::string &path,int status);
-}
+  };
+  std::pair<Methods::error, Response > Get(Request &request, std::string path);
+  std::pair<Methods::error, Response > Delete(std::string path);
+  ssize_t                              getFile(std::string &path, int status);
+} // namespace libhttp
 std::string generateTemplate(std::string &path);
